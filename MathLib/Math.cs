399b37763dbe0ebe8a220cb81648d15d54a35193
Jada <jada.fagan@gmail.com>
2020-06-25 11:52:43 -0400

    Add project files.

---
+++ MathLib/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MathLib
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
+}
---
+++ TestMathLib/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace TestMathLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 'math' is declared with var, other side of '=' is initializing it. All other lines are using it
+            var math = new MathLib.Math();
+            Console.WriteLine(math);
+            var ans = math.Add(1, 2);
+            ans = math.Subtract(5, 2);
+
+
+        }
+
+
+
+    }
+}
